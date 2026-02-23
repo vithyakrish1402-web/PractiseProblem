1cca202b1c0a604e533b255852bed94c63ddf66f
Vithya Krishnan R A <vithyakrish1402@gmail.com>
2026-02-23 11:25:04 +0530

    done

---
+++ Level1Qn1.java
@@ -0,0 +1,12 @@
+// Program to calculate Harry's age
+public class HarryAge {
+    public static void main(String[] args) {
+
+        int birthYear = 2000;
+        int currentYear = 2024;
+
+        int age = currentYear - birthYear;
+
+        System.out.println("Harry's age in " + currentYear + " is " + age);
+    }
+}
